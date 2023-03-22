5fb0c8c0adc3c00b7676940c0a08f4d5f0bda149
Maycon Lima <90663036+maycovisk@users.noreply.github.com>
2023-03-22 14:12:19 -0300

    Add Exercicio5CS

---
+++ Exercicio5CS/Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string original, invertida = "";
+        Console.WriteLine("Digite uma palavra:");
+        original = Console.ReadLine();
+        for (int i = original.Length - 1; i >= 0; i--)
+        {
+            invertida += original[i];
+        }
+        Console.WriteLine("Palavra invertida: {0}", invertida);
+    }
+}
