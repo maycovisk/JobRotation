00b631a4b2bd03c8c1e8b1c480e8d944f8282daa
Maycon Lima <90663036+maycovisk@users.noreply.github.com>
2023-03-22 09:52:28 -0300

    Add Exercicio2 em C#

---
+++ Exercicio2CS/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.Write("Digite um número inteiro positivo: ");
+        num = int.Parse(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        while (c < num)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        if (c == num)
+        {
+            Console.WriteLine(num + " pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine(num + " não pertence à sequência de Fibonacci.");
+        }
+    }
+}
